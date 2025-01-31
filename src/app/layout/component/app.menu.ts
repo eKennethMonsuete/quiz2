61a4cb9d4ea27ab20049e020b211ae297df9d3da
Kenneth <keeentt19@gmail.com>
2025-01-31 12:35:29 -0300

    rota quiz

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -27,6 +27,11 @@ export class AppMenu {
             {
                 label: 'UI Components',
                 items: [
+                    {
+                        label: 'Quiz Tema',
+                        icon: 'pi pi-fw pi-bars',
+                        routerLink: ['/pages/quiz']
+                    },
                     { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
                     { label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input'] },
                     { label: 'Button', icon: 'pi pi-fw pi-mobile', class: 'rotated-icon', routerLink: ['/uikit/button'] },
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Documentation } from './documentation/documentation';
 import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
+import { Quiz1Component } from './quiz1/quiz1.component';
 
 export default [
     { path: 'documentation', component: Documentation },
     { path: 'crud', component: Crud },
     { path: 'empty', component: Empty },
+    { path: 'quiz', component: Quiz1Component },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
