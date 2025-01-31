6e1e5d3e684bb47c785a43de365780e1084f9e2f
Kenneth <keeentt19@gmail.com>
2025-01-31 11:17:37 -0300

    init quis master

---
+++ src/app/pages/quiz1/quiz1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz1',
+  imports: [],
+  templateUrl: './quiz1.component.html',
+  styleUrl: './quiz1.component.scss'
+})
+export class Quiz1Component {
+
+}
